4ea8fe61532832b894fdfb5f855131e58a993340
mason.hows11 <mason.hows11@gmail.com>
2023-01-31 06:02:35 +0330

    first commit

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,15 @@
+<template>
+   <h1>
+       How To Install Vue 3 in Laravel 9 with Vite
+   </h1>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style scoped>
+
+</style>
