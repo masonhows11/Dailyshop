03e3bfd5d09c994b802a1f7c70b633c3a6f6f4aa
mason.hows11 <mason.hows11@gmail.com>
2023-02-02 06:24:57 +0330

    admin guard updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +25,84 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        $admin = Admin::create([
+            'name' => 'naeem_sol',
+            'first_name' => 'naeem',
+            'last_name' => 'soltany',
+            'mobile' => '09917230927',
+            //'token'=>  mt_rand(111111,999999),
+            //'token_verified_at' => Carbon::now(),
+        ]);
+        $admin1 = Admin::create([
+            'name' => 'james_423',
+            'first_name' => 'joe',
+            'last_name' => 'james',
+            'mobile' => '09172890423',
+            //'token'=>  mt_rand(111111,999999),
+            //'token_verified_at' => Carbon::now(),
+        ]);
+
+        $role_admin = Role::create(['guard_name' => 'admin', 'name' => 'admin']);
+        $admin->assignRole($role_admin);
+
+        // create users
+        $user = User::create([
+            'name' => 'mason11',
+            'first_name' => 'mason',
+            'last_name' => 'howsHows',
+            'email' => 'mason.hows11@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('1289..')
+        ]);
+
+        $user1 = User::create([
+            'name' => 'sara1992',
+            'first_name' => 'sara',
+            'last_name' => 'redField',
+            'email' => 'sara1992@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('1289..'),
+        ]);
+
+        $users = [
+            [
+                'name' => 'nicky',
+                'first_name' => 'nick',
+                'last_name' => 'wilson',
+                'email' => 'nicky.wilson21@gmail.com',
+                'password' => Hash::make('1289..//**'),
+                'mobile' => '09917230929',
+            ],
+            [
+                'name' => 'Mary',
+                'first_name' => 'maria',
+                'last_name' => 'watson',
+                'email' => 'mary.watson90@gmail.com',
+                'password' => Hash::make('1289..//**'),
+                'mobile' => '09917230925',
+            ],
+            [
+                'name' => 'John97',
+                'first_name' => 'John',
+                'last_name' => 'marston',
+                'email' => 'john.marston1870@gmail.com',
+                'password' => Hash::make('1289..//**'),
+                'mobile' => '09917230922',
+            ],
+            [
+                'name' => 'David',
+                'first_name' => 'David120',
+                'last_name' => 'Bombal',
+                'email' => 'david.bombal11@gmail.com',
+                'password' => Hash::make('1289..//**'),
+                'mobile' => '09917230911',
+            ],
+
+        ];
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
