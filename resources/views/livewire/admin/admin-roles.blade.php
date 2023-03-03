4106a2447b5d0793527747e5730f4735afcd3901
mason.hows11 <mason.hows11@gmail.com>
2023-03-03 10:42:08 +0330

    ready for add toast msg for delete roles & perms

--- resources/views/livewire/admin/admin-roles.blade.php
+++ resources/views/livewire/admin/admin-roles.blade.php
@@ -109,4 +109,15 @@
             });
         })
     </script>
+    <script>
+        @if(session()->has('success'))
+        Toastify({
+            text: "{{ session('success')  }}",
+            className: "info",
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            }
+        }).showToast();
+        @endif
+    </script>
 @endpush
